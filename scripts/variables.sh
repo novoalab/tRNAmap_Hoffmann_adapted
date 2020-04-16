7c6d9eadb710c5a80b909ba71f48c02231f63e94
soniacruciani <44866316+soniacruciani@users.noreply.github.com>
2020-04-16 11:10:57 +0200

    Update variables.sh

--- scripts/variables.sh
+++ scripts/variables.sh
@@ -6,15 +6,15 @@ project=yourprojectfolderpath (that will always be the folder from which you qsu
 
 export RUN="singularity exec -e $project/scripts/trna_align-100120.simg"
 
-bbduk="$RUN bbduk.sh" #BBMap version 36.14
-fastqc="$RUN fastqc" #v0.11.4
-samtools="$RUN samtools" #1.3 (using htslib 1.3)
-tRNAscanSE="$RUN tRNAscan-SE" #1.3.1 (January 2012)
-bedtools="$RUN bedtools" #v2.25.0
-segemehl="$RUN segemehl.x" #0.2.0-418
-picardJar="picard.jar" #2.2.1
-picard="$RUN java -jar $picardJar"
-gatkJar="GenomeAnalysisTK.jar" #3.5-0-g36282e4
+bbduk="$RUN bbduk.sh"                     #BBMap version 36.14
+fastqc="$RUN fastqc"                      #v0.11.8
+samtools="$RUN samtools"                  #1.3 (using htslib 1.3)
+tRNAscanSE="$RUN tRNAscan-SE"             #2.0.4 (September 2019)(infernal 1.1.2)
+bedtools="$RUN bedtools"                  #v2.25.0
+segemehl="$RUN segemehl.x"                #0.2.0-418
+picardJar="picard.jar"                    #required by hoffmann is 2.2.1
+picard="$RUN java -jar $picardJar"         
+gatkJar="GenomeAnalysisTK.jar"            #required by hoffmann is 3.5-0-g36282e4
 gatk="$RUN java -jar $gatkJar"
 
 #directory paths:
