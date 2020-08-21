eaddfdfd14ca2c4bb6fc84dddaaa9d298966eeb6
soniacruciani <44866316+soniacruciani@users.noreply.github.com>
2020-08-21 12:03:30 +0200

    Create keepprecursors.sh

---
+++ scripts/keepprecursors.sh
@@ -0,0 +1,12 @@
+#directory paths
+cwd=$(pwd)
+
+source ${cwd}/scripts/variables.sh
+
+bn=$(basename $n _filtered.fastq.gz)
+
+cd ${workDir}/mapping
+zcat ${bn}.fastq.gz | paste  - - - - | sed 's/@/@\t/g' | cut -f 2 > ${bn}_matureIDs.txt
+
+samtools view ${bn}_filtered.sam | grep -vf ${bn}_matureIDs.txt | samtools view -Sb | samtools sort > ${bn}_pre-tRNAs.bam
+samtools index ${bn}_pre-tRNAs.bam
